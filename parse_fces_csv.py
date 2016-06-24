0520fb2788cafac0e96bd0eb40b00fa3fd477340
justinchuby <justinchuby@foxmail.com>
2016-06-24 15:17:13 -0400

    'Section' is now string
    #1

--- parse_fces_csv.py
+++ parse_fces_csv.py
@@ -72,14 +72,9 @@ def normalize_fce(data):
             newQuestions = {}
             for key, value in questions.items():
                 match = re.search('(\d+):', key)
-                i = 100
                 if match:
                     questionNum = match.group(1)
                     questionBody = re.sub('(\d+):', '', key).strip()
-                else:
-                    questionNum = str(i)
-                    questionBody = key
-                    i += 1
                 newQuestions[questionNum] = {'body': questionBody,
                                              'value': value}
             newDoc['questions'] = newQuestions
@@ -104,7 +99,7 @@ def parse_table(table):
         cells = row
 
         # Skip cells containing "Year of". That's the summary of the year.
-        if 'Year of' in cells[5]:
+        if cells[0] == '' or 'Year of' in cells[5]:
             continue
 
         # Columns are not consistent in a table - update them when
@@ -140,7 +135,7 @@ def parse_table(table):
                             if len(value) == 4:
                                 value = '0' + value
                             value = value[:2] + '-' + value[2:]
-                        else:
+                        elif columns[index] != 'Section':
                             value = int(value)
                     elif index == question_start - 1:
                         try:
